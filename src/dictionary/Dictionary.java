0abcb07d48872af8f5b321085c0ae59eaa5e486e
Jai Deep <jaideepp247@gmail.com>
2023-01-08 00:51:39 +0500

    An English Dictionary in Java GUI By using HashMap. Platform: Netbeans 8.2

---
+++ src/dictionary/Dictionary.java
@@ -0,0 +1,16 @@
+
+package dictionary;
+import java.util.HashMap;
+import java.io.*;
+
+
+    
+     
+public class Dictionary {
+    public static void main(String[] args) throws Exception {
+        // TODO code application logic here
+        
+         
+    }
+    
+}
